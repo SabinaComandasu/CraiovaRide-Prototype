cb12cc637f9fa6042beb85ff9cb46cfb60da8e14
Darius762 <diculescu.darius.robert@gmail.com>
2025-04-26 22:29:15 +0300

    front end pentru tabul rides

---
+++ Proiect - Implementare Software/Services/IRideService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections.Generic;
+using Proiect_Implementare_Software.Models;
+
+namespace Proiect_Implementare_Software.Services
+{
+    public interface IRideService
+    {
+        IEnumerable<Ride> GetUpcomingRides();
+        IEnumerable<Ride> GetCompletedRides();
+    }
+}
---
+++ Proiect - Implementare Software/Services/RideService.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Proiect_Implementare_Software.Data;
+using Proiect_Implementare_Software.Models;
+
+namespace Proiect_Implementare_Software.Services
+{
+    public class RideService : IRideService
+    {
+        private readonly AppDbContext _context;
+
+        public RideService(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public IEnumerable<Ride> GetUpcomingRides()
+        {
+
+            return _context.Rides.Where(r => r.RideStatus == "Scheduled").ToList();
+        }
+
+        public IEnumerable<Ride> GetCompletedRides()
+        {
+    
+            return _context.Rides.Where(r => r.RideStatus == "Completed").ToList();
+        }
+    }
+}
