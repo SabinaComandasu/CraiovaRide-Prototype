84dbfcece4b66171019533f1adf125c2d8d901e2
Darius762 <diculescu.darius.robert@gmail.com>
2025-05-05 00:05:18 +0300

    update account feature

---
+++ Proiect - Implementare Software/Services/AccountService.cs
@@ -0,0 +1,25 @@
+﻿using Proiect_Implementare_Software.Models;
+using Proiect_Implementare_Software.Data;
+
+namespace Proiect_Implementare_Software.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _repo;
+
+        public AccountService(IAccountRepository repo)
+        {
+            _repo = repo;
+        }
+
+        public async Task<Person?> GetUserAsync(int id)
+        {
+            return await _repo.GetUserByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Person>> GetAllUsersAsync()
+        {
+            return await _repo.GetAllUsersAsync();
+        }
+    }
+}
---
+++ Proiect - Implementare Software/Services/IAccountService.cs
@@ -0,0 +1,11 @@
+﻿using Proiect_Implementare_Software.Models;
+using System.Threading.Tasks;
+
+namespace Proiect_Implementare_Software.Services
+{
+    public interface IAccountService
+    {
+        Task<Person?> GetUserAsync(int id);
+        Task<IEnumerable<Person>> GetAllUsersAsync();
+    }
+}
