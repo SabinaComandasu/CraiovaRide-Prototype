c21e927549b36c43e4096bc4d8343fb9c746cdb5
DESKTOP-CRJ70FL\SABINA <comandasu.sabina.x9g@student.ucv.ro>
2025-05-06 07:48:25 +0300

    Aborted rebase

--- Proiect - Implementare Software/obj/Debug/net8.0/Proiect - Implementare Software.AssemblyInfo.cs
+++ Proiect - Implementare Software/obj/Debug/net8.0/Proiect - Implementare Software.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proiect - Implementare Software")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9a0d5f5797e5c04663efa285ce65e7996f388cc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e00b5d5957fede514c5f47e081c8591cca37c6bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proiect - Implementare Software")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proiect - Implementare Software")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
