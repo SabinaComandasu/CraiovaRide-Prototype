3a47e05059b353568690b3710112535ba06288ff
DESKTOP-CRJ70FL\SABINA <comandasu.sabina.x9g@student.ucv.ro>
2025-05-06 13:41:09 +0300

    Added "Return to Log In Page" Button after registering an account.

--- Proiect - Implementare Software/obj/Debug/net8.0/Proiect - Implementare Software.AssemblyInfo.cs
+++ Proiect - Implementare Software/obj/Debug/net8.0/Proiect - Implementare Software.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proiect - Implementare Software")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d0150333c8d95b5b4595404395eb13439bc12d07")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c788284ae949ed095b123ba3c03ae92e5ec21530")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proiect - Implementare Software")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proiect - Implementare Software")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
