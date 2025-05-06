f0024fa265586752d403f3c8a984f486ec95798b
DESKTOP-CRJ70FL\SABINA <comandasu.sabina.x9g@student.ucv.ro>
2025-05-06 17:47:44 +0300

    Added and implemented Logout option to the sidebar. Reformatted the sidebar.

--- Proiect - Implementare Software/Controllers/HomeController.cs
+++ Proiect - Implementare Software/Controllers/HomeController.cs
@@ -1,23 +1,48 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Proiect_Implementare_Software.Models;
+using Proiect_Implementare_Software.Data;
 using Proiect_Implementare_Software.Services;
 using System.Diagnostics;
 
+namespace Proiect_Implementare_Software.Controllers
 {
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
         private readonly IHomeService _homeService;
+        private readonly AppDbContext _context;
+        private readonly UserManager<IdentityUser> _userManager;
 
-        public HomeController(ILogger<HomeController> logger, IHomeService homeService)
+        public HomeController(
+            ILogger<HomeController> logger,
+            IHomeService homeService,
+            AppDbContext context,
+            UserManager<IdentityUser> userManager)
         {
             _logger = logger;
             _homeService = homeService;
+            _context = context;
+            _userManager = userManager;
         }
 
-        public IActionResult Index()
+        public async Task<IActionResult> Index()
         {
-            ViewBag.Welcome = _homeService.GetWelcomeMessage();
+            var user = await _userManager.GetUserAsync(User);
+
+            if (user != null)
+            {
+                var person = _context.Persons.FirstOrDefault(p => p.IdentityUserId == user.Id);
+                if (person != null)
+                {
+                    ViewBag.AvatarPath = "/images/" + person.Avatar;
+                    ViewBag.FullName = person.FullName;
+                    ViewBag.Rating = person.Rating.ToString("0.0") ?? "N/A";
+                }
+            }
+
+
             return View();
         }
 
