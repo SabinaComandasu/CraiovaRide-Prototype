d0150333c8d95b5b4595404395eb13439bc12d07
DESKTOP-CRJ70FL\SABINA <comandasu.sabina.x9g@student.ucv.ro>
2025-05-06 13:25:52 +0300

    Styled RegisterConfirmation.cshtml.

--- Proiect - Implementare Software/obj/Debug/net8.0/Proiect - Implementare Software.AssemblyInfo.cs
+++ Proiect - Implementare Software/obj/Debug/net8.0/Proiect - Implementare Software.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proiect - Implementare Software")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+71d03dd3429b62ca46e18e01de7d9c5d469fa7d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f087f54c1cd35fef1414bfe2b64d3bab7ff144bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proiect - Implementare Software")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proiect - Implementare Software")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
