30edda34ae11f35791a126e2d466a48e6140af31
DESKTOP-CRJ70FL\SABINA <comandasu.sabina.x9g@student.ucv.ro>
2025-05-06 14:38:18 +0300

    Added sound effects for correct & incorrect credentials.

--- Proiect - Implementare Software/obj/Debug/net8.0/Proiect - Implementare Software.AssemblyInfo.cs
+++ Proiect - Implementare Software/obj/Debug/net8.0/Proiect - Implementare Software.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Proiect - Implementare Software")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3a47e05059b353568690b3710112535ba06288ff")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+43eeb8293a0343d75681796b1bdbd82f9d50a9c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Proiect - Implementare Software")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Proiect - Implementare Software")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
