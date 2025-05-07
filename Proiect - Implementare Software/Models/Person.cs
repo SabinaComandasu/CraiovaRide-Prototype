8fa72eb2c8a1fe379334fd51982e24fec3ec44a9
Darius762 <diculescu.darius.robert@gmail.com>
2025-05-07 19:34:40 +0300

    update account edit info

--- Proiect - Implementare Software/Models/Person.cs
+++ Proiect - Implementare Software/Models/Person.cs
@@ -3,19 +3,19 @@
     public class Person
     {
         public int PersonID { get; set; }
-        public string IdentityUserId { get; set; }
+        public string IdentityUserId { get; set; } 
 
-        public string Username { get; set; }
-        public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
+        public string Username { get; set; } 
+        public string FullName { get; set; } 
+        public string PhoneNumber { get; set; } 
         public string? Avatar { get; set; }
         public float Rating { get; set; }
         public bool Availability { get; set; }
-        public string DriverStatus { get; set; }
-        public string Role { get; set; }
+        public string DriverStatus { get; set; } 
+        public string Role { get; set; } 
 
         public int? VehicleID { get; set; }
-        public Vehicle Vehicle { get; set; }
+        public Vehicle Vehicle { get; set; }    
 
         // Relații clare și corecte
         public ICollection<UserOrdersRide> OrderedRides { get; set; }  // asta e important
